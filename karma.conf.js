5f14a870dc1fd14a23f560dc1259c52bed60a01b
Sébastien Vermeille <sebastien.vermeille@gmail.com>
2017-03-04 10:01:31 +0100

    Fix travis

--- karma.conf.js
+++ karma.conf.js
@@ -102,6 +102,7 @@ module.exports = function(config) {
   if (process.env.TRAVIS) {
     cfg.autoWatch = false;
     cfg.browsers = ['Chrome_travis_ci'];
-    config.set(cfg);
   }
+
+  config.set(cfg);
 };
